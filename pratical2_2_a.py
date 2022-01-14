da4cc150f332235c123539e88f8ade3518001216
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:39:21 +0530

    Add files via upload

---
+++ pratical2_2_a.py
@@ -0,0 +1,10 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-2.a.git
+#Aim: Write a Python program to create a tuple with different data types.
+tuple=(1,2,3,4,5)
+tuple1=("red","yellow","green")
+tuple2=('true','false','true')
+print(tuple)
+print(tuple1)
+print(tuple2)
